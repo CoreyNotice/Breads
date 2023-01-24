23ac3308f664bc431f8a92932aa7488b899b8826
Corey N <coreynotice@yahoo.com>
2023-01-23 20:30:52 -0500

    things

---
+++ views/index.jsx
@@ -0,0 +1,26 @@
+const React=require('react')
+const Default=require('./layout/Default')
+
+function Index({breads,title}){
+    return(
+        <Default title={title}>
+            <h2>Index Page</h2>
+            {/* <p>Get your {breads[0].name} bread here!</p> */}
+            <ul>
+                {
+                    breads.map((bread,index)=>{
+                        return(<li key={index}>
+                            <a href={`/bread/${index}`}>
+                                {bread.name}
+                            </a>
+                        </li>
+                        )
+                    })
+                }
+
+            </ul>
+        </Default>
+    )
+}
+
+module.exports=Index
